270b17b59a21f7ae6286cb6e1a88ca28022f1cbc
Tim Booher <tim@theboohers.org>
2015-02-12 21:36:44 -0500

    added logos

--- sidebar-community.php
+++ sidebar-community.php
@@ -3,4 +3,6 @@
     <li><?php partnerUrl('http://dc.mypowersupply.com/','powersupply-crest.png'); ?></li>
     <li><?php partnerUrl('http://www.thejacksonclinics.com/services/health/crossfit-program','jackson-clincs.jpg'); ?></li>
     <li><?php partnerUrl('https://www.customfitmeals.com/order-meals','custom-fit-meals.gif'); ?></li>
+    <li><?php partnerUrl('http://www.facebook.com/kxphotos', 'kx.gif') ?></li>
+    <li><?php partnerUrl('http://www.theboxpass.com', 'box_pass.png') ?></li>
 </ul>
