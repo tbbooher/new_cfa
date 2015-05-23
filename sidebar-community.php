4053278f4fadbd27dd66ff9fd24ff8f678a16d18
Tim Booher <tim@theboohers.org>
2015-05-23 06:10:11 -0400

    modd kx logo

--- sidebar-community.php
+++ sidebar-community.php
@@ -3,7 +3,7 @@
     <li><?php partnerUrl('http://dc.mypowersupply.com/','powersupply-crest.png'); ?></li>
     <li><?php partnerUrl('http://www.thejacksonclinics.com/services/health/crossfit-program','jackson-clincs.jpg'); ?></li>
     <li><?php partnerUrl('https://www.customfitmeals.com/order-meals','custom-fit-meals.gif'); ?></li>
-    <li><?php partnerUrl('http://www.kxphotos.com', 'kx.gif') ?></li>
+    <li><?php partnerUrl('http://www.kxphotos.com', 'kx.png') ?></li>
     <li><?php partnerUrl('http://www.theboxpass.com', 'box_pass.png') ?></li>
     <li><a href='http://www.shareasale.com/r.cfm?b=326747&amp;u=662935&amp;m=34288&amp;urllink=&amp;afftrack='><img src='http://www.shareasale.com/image/34288/125x125_performance.jpg'></a></li>
 </ul>
