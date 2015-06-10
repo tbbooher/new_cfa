0006460570a7143fa7611cda5e3ee43371a538dc
Christine Booher <christinebooher@Christines-MacBook-Air.local>
2015-06-10 06:23:28 -0400

    updated

--- sidebar-community.php
+++ sidebar-community.php
@@ -1,9 +1,4 @@
 <h3>Partners</h3>
 <ul class="community">
-    <li><?php partnerUrl('http://dc.mypowersupply.com/','powersupply-crest.png'); ?></li>
-    <li><?php partnerUrl('http://www.thejacksonclinics.com/services/health/crossfit-program','jackson-clincs.jpg'); ?></li>
-    <li><?php partnerUrl('https://www.customfitmeals.com/order-meals','custom-fit-meals.gif'); ?></li>
-    <li><?php partnerUrl('http://www.kxphotos.com', 'kx.png') ?></li>
-    <li><?php partnerUrl('http://www.theboxpass.com', 'box_pass.png') ?></li>
     <li><a href='http://www.shareasale.com/r.cfm?b=326747&amp;u=662935&amp;m=34288&amp;urllink=&amp;afftrack='><img src='http://www.shareasale.com/image/34288/125x125_performance.jpg'></a></li>
 </ul>
